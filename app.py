474b73cd98c6d5d0f8522b5376df3144cccdbcb7
SooHoon Choi <soohoonchoi@gmail.com>
2023-12-02 13:37:37 -0800

    .

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_sockets import Sockets
+from flask_socketio import SocketIO
 from elevenlabs.api.tts import TTS, Voice, Model
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
@@ -9,11 +9,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-sockets = Sockets(app)
+socketio = SocketIO(app)
 
-# @app.route('/')
-# def index():
-#     return 'Hello World!'
+@app.route('/')
+def index():
+    return 'Hello World!'
 
 # Ensure you have the necessary ElevenLabs API key set as an environment variable
 @app.route('/tts', methods=['POST'])
@@ -31,14 +31,21 @@ def tts():
     # Return a response to upgrade to WebSocket
     return jsonify({"message": "Please connect to WebSocket to receive the audio stream."})
 
-@sockets.route('/test')
-def test_socket(ws):
-    print('test socket')
-    message = ws.receive()
-    print(message)
-    ws.send('message received: ' + message)
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
 
-@sockets.route('/tts/socket')
+# @socketio.on('connect')
+# def test_socket(ws):
+#     print("test websocket")
+#     try:
+#         while not ws.closed:
+#             message = ws.receive()
+#             if message:
+#                 print("Received message from VS Code:", message)
+#     except Exception as e:
+#         print("Exception in test_socket:", e)
+
 def tts_socket(ws):
     # Extract text from WebSocket message
     message = ws.receive()
@@ -63,7 +70,10 @@ def tts_socket(ws):
     for audio_chunk in audio_stream:
         ws.send(audio_chunk)
 
+# if __name__ == '__main__':
+#     server = pywsgi.WSGIServer(('127.0.0.1', 5000), app, handler_class=WebSocketHandler)
+#     print("Server listening on: http://127.0.0.1:5000")
+#     socketio.run(app)
+
 if __name__ == '__main__':
-    server = pywsgi.WSGIServer(('127.0.0.1', 5000), app, handler_class=WebSocketHandler)
-    print("Server listening on: http://127.0.0.1:5000")
-    server.serve_forever()
+    socketio.run(app)
